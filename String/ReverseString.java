174b3baf9e8b6cc81dd13d730c1175c276ea294e
Vivek <vivekraj78965.vr@gmail.com>
2023-09-28 17:34:55 +0530

    ReverseString(method 1)

---
+++ String/ReverseString.java
@@ -0,0 +1,24 @@
+package String;
+
+// Method 1. By Concatenating
+import java.util.Scanner;
+public class ReverseString {
+    
+    public static String reverse(String str){
+        String revStr = "";
+        int len = str.length();
+        for(int i = len-1; i>=0 ; i--) {
+            revStr = revStr + str.charAt(i);
+        }
+        return revStr;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter The String");
+        String input = sc.nextLine();
+        String reversedString = reverse(input);
+        System.out.println("The Reversed String Is:" + " " + reversedString); 
+    }
+
+}
