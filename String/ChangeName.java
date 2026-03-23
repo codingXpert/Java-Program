e7e33cd6a98dcb66612dfed5a93b5a680ddf5c76
codingXpert <vivekraj78965.vr@gmail.com>
2026-03-23 08:37:15 +0530

    change name

---
+++ String/ChangeName.java
@@ -0,0 +1,11 @@
+public class ChangeName {
+    public static void main(String[] args) {
+        String name = "Kunal Kushwaha";
+        changeName(name);
+        System.out.println(name);
+    }
+
+    public static void changeName(String name) {
+        name = "Rahul Rana";
+    }
+}
