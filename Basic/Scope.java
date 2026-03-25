25365367beba44bb478cc9e32e8eda6680a9f7b4
codingXpert <vivekraj78965.vr@gmail.com>
2026-03-25 07:52:12 +0530

    Basic scoping

---
+++ Basic/Scope.java
@@ -0,0 +1,39 @@
+
+public class Scope {
+
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        String name = "Kunal";
+
+        // Block Scope
+        {
+            //int a = 78; // already initialised outside the block in the same method, hence we cannot initialise again
+            a = 100; // reassign the origin ref variable to some other value
+            System.out.println(a);
+            int c = 99;
+            name = "Rahul";
+            System.out.println(name);
+            // values initialised in this block, will remain in this block only
+        }
+
+        int c = 900; // we can not initilize this c on line no 8. As it will start giving error that c is already initilize. But we can initilize the same variables again after the block scope ends.
+        System.out.println(a);
+        System.out.println(name);
+//        System.out.println(c); // cannot use outside the block
+
+        // scoping in for loops
+        for (int i = 0; i < 4; i++) {
+//            System.out.println(i);
+            int num = 90;
+            a = 10000;
+        }
+        System.out.println();
+    }
+
+    static void random(int marks) {
+        int num = 67;
+        System.out.println(num);
+        System.out.println(marks);
+    }
+}
